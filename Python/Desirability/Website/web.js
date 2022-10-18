745a494c53a6e81ed80eb071608383984c036095
Josh-Linux <dupetopetoman@gmail.com>
2022-10-18 13:04:12 +0200

    Did some english reports

---
+++ Python/Desirability/Website/web.js
@@ -0,0 +1,5 @@
+let surveyBox = document.getElementById('survey_box');
+
+
+surveyBox.onmouseover = function () {surveyBox.style.boxShadow = '0 0 5px 5px blue'};
+surveyBox.onmouseout = function () {surveyBox.style.boxShadow = 'none'};
